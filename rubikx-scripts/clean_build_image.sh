48106304296e8414004d372c99c948a5639ec11a
periatrubikx <V4venkata!>
2021-09-20 00:30:41 -0500

    Updated script file; Updated NodeRED url.

--- rubikx-scripts/clean_build_image.sh
+++ rubikx-scripts/clean_build_image.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-docker rmi -f rubikx/iot-central:3.3.0
+docker rmi -f rubikx/iot-central:3.3.2-SNAPSHOT
 
-#mvn clean install -DskipTests=true -Dlicense.skip=true
+cd ..
 
-docker build -t rubikx/iot-central:3.3.0 .
+source ~/.profile
+
+java11
+
+mvn clean install -DskipTests=true -Dlicense.skip=true
+
+docker build -t rubikx/iot-central:3.3.2-SNAPSHOT .
